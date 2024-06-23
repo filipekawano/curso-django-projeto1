1b4645a04b57d32238a5fe17c0fef253f4598483
Filipe Kawano <filipekawan@hotmail.com>
2024-06-23 19:39:14 -0300

    Exemplo de Template

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,17 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 2')
-    # return HTTP Response
+    return render(request, 'recipes/home.html', context={
+        'name': 'Filipe Kawano'
+    })
 
 
 def contato(request):
     return HttpResponse('contato')
-    # return HTTP Response
 
 
 def sobre(request):
     return HttpResponse('sobre')
-    # return HTTP Response
 
